a3ecb48d9f49267929daa8f56ec354333500ce4d
mu-jchung <chungjodad@gmail.com>
2015-11-20 14:47:32 -0500

    Initial commit

---
+++ core/src/com/badlogic/drop/State.java
@@ -0,0 +1,9 @@
+package com.badlogic.drop;
+
+/**
+ * Created by chungj on 11/20/15.
+ * Per http://stackoverflow.com/questions/21576181/pause-resume-a-simple-libgdx-game-for-android
+ */
+public enum State{
+    RUN, PAUSE
+}
